90bf8df3e6a1b67b2d25ad18de2104b3e75699d7
Blxee <aminedev57@gmail.com>
2023-06-21 15:39:48 +0100

    Changed: reverted pop implementation

--- instructions1.c
+++ instructions1.c
@@ -67,19 +67,13 @@ void pint(stack_t **stack, unsigned int line_number)
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
-
 	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
 		exit_program(EXIT_FAILURE);
 	}
 
-	temp = *stack;
-	*stack = (*stack)->next;
-	if (*stack)
-		(*stack)->prev = NULL;
-	free_mem(temp);
+	pop_stack(stack);
 }
 
 /**
