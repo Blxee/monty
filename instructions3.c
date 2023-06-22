bb4f8a0d27dadd708fa8403fca342512d46fbc1a
Blxee <aminedev57@gmail.com>
2023-06-22 12:57:30 +0100

    Added: the `pchar` instruction

--- instructions3.c
+++ instructions3.c
@@ -29,3 +29,31 @@ void mod(stack_t **stack, unsigned int line_number)
 
 	push_stack(stack, second % first);
 }
+
+/**
+ * pchar - the "pchar" instruction implementation
+ *
+ * @stack: the main stack to operate on
+ * @line_number: line number of the current instruction
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	int n;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr,
+				"L%u: can't pchar, stack empty\n", line_number);
+		exit_program(EXIT_FAILURE);
+	}
+
+	n = (*stack)->n;
+	if (n < 0 || n > 126)
+	{
+		fprintf(stderr,
+			"L%u: can't pchar, value out of range\n", line_number);
+		exit_program(EXIT_FAILURE);
+	}
+
+	printf("%c\n", n);
+}
--- monty.c
+++ monty.c
@@ -41,13 +41,13 @@ void fill_instructions(instruction_t list[])
 		"push", "pall", "pint",
 		"pop", "swap", "add",
 		"nop", "sub", "div",
-		"mul", "mod"
+		"mul", "mod", "pchar"
 	};
 	void (*funcs[])(stack_t **, unsigned int) = {
 		push, pall, pint,
 		pop, swap, add,
 		nop, sub, _div,
-		mul, mod
+		mul, mod, pchar
 	};
 	unsigned int i;
 
@@ -69,7 +69,7 @@ void fill_instructions(instruction_t list[])
 int main(int argc, char *argv[])
 {
 	stack_t *stack = NULL;
-	instruction_t instruction_list[11];
+	instruction_t instruction_list[12];
 	FILE *file;
 	size_t line_alloc = 0, line_number = 0;
 	ssize_t line_len;
